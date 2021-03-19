804489d9a1fd38eb7670d71675716c964ab070f0
Dirk Mueller <dirkmuller8@outlook.de>
2021-03-19 07:05:02 +0100

    Added amount of guesses and handled the upper case issue

--- Aufgabe_8/MainWindow.xaml.cs
+++ Aufgabe_8/MainWindow.xaml.cs
@@ -16,31 +16,34 @@ using System.Windows.Shapes;
 namespace Aufgabe_8
 {
     // Author: Dirk Mueller
-    // Date: 16.03.2021
+    // Date: 19.03.2021
     //
     // Algorithm:
-    // 1. Take the input from the user and check the validity of it
-    // 2. Each row (starting with 1) to be printed is a combination of spaces and star symbols
-    // 3. There are spaces as much as triangle height minus row number
-    // 4. The amount of stars is 1, 3, 5, 7,... in rows 1, 2, 3, 4, 5, respectively
-    //
+    // 1. Create random word
+    // 2. Have user give a letter
+    // 3. Check the existence of the letter in the word
+    // 4. If the letter exists replace it by a star and display it on screen
+    // 5. When all letters are guessed inform the use
+    // 6. If more than 10 guesses were place abort the game
 
     public partial class MainWindow : Window
     {
         List<string> wordList = new List<string>
         { "Wetter", "Milch", "Datum", "Pflanze", "Ampel", "Telefon", "Toast", "Abitur", "Kante", "Wippe"};
-        List<int> charPositionList = new List<int>();
+        //List<int> charPositionList = new List<int>();
         Random random = new Random();
-        //char[] charArray = new char[];
         int randomNumber;
         string word;
         string stars = "";
+        static int AmountOfGuesses = 0;
 
         public MainWindow()
         {
             InitializeComponent();
+            randomNumber = random.Next(1, 11);
+            // Pick word randomly:
             word = wordList[randomNumber];
-            lblFinished.Content = word; ;
+            lblFinished.Content = word;
             for (int i = 0; i < word.Length; i++)
             {
                 stars += "*";
@@ -50,7 +53,6 @@ namespace Aufgabe_8
 
         private void btnNewWord_Click(object sender, RoutedEventArgs e)
         {
-            randomNumber = random.Next(1, 11);
             StringBuilder sb = new StringBuilder("", word.Length);
             for (int i = 0; i < word.Length; i++)
             {
@@ -81,24 +83,30 @@ namespace Aufgabe_8
             char[] charWord = new char[word.Length];
             //char eingabe;
             string starsReplaced;
-            int AmountOfGuesses = 0;
 
             for (int i = 0; i < word.Length; i++)
             {
                 charWord[i] = word[i];
             }
 
-            starsReplaced = ReplaceByLetterFound(letterGuessed, stars, charWord);
-            txtWordDisplay.Text = starsReplaced;
-
-            if (AllStarsReplacedByLetters(starsReplaced))
+            if (AmountOfGuesses <= 10)
             {
-                lblFinished.Content = "Congratulations! You did it in " + AmountOfGuesses + " guesses";
-                stars = "";
-                word = "";
+                starsReplaced = ReplaceByLetterFound(letterGuessed, stars, charWord);
+                txtWordDisplay.Text = starsReplaced;
+
+                if (AllStarsReplacedByLetters(starsReplaced))
+                {
+                    lblFinished.Content = "Congratulations! You did it in " + AmountOfGuesses + " guesses";
+                    stars = "";
+                    word = "";
+                }
+                // Replace star sequence by newly formed sequence:
+                stars = starsReplaced;
             }
-            // Replace star sequence by newly formed sequence:
-            stars = starsReplaced;
+            else
+            {
+                MessageBox.Show("Sorry, you have exceeded the limit of 10 guesses");
+            }
         }
 
         static string ReplaceByLetterFound(char letter, string stars, char[] word)
@@ -107,11 +115,12 @@ namespace Aufgabe_8
             for (int i = 0; i < word.Length; i++)
             {
                 // If 
-                if (word[i] == letter)
+                if (Char.ToUpper(word[i]) == Char.ToUpper(letter))
                 {
-                    sb[i] = letter;
+                    sb[i] = word[i];
                 }
             }
+            AmountOfGuesses++;
             return sb.ToString();
         }
 
